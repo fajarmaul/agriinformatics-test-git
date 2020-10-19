9d138389ab30c820e948e1bda3dc0ff8e4bbb581
Fajar Maulana <fajar.maulana@digiasia.asia>
2020-10-20 03:15:46 +0700

    Changes #1

--- app/src/main/java/com/mad/testgit2/MainActivity.java
+++ app/src/main/java/com/mad/testgit2/MainActivity.java
@@ -10,5 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Changes
+        // I will change here #1
     }
 }
